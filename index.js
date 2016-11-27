fe847ee7d47b4467247f43b4bee296a145562fb1
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-26 21:48:27 -0700

    add basic routes--need further support on more complex queries

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
 'use strict';
 
-const env='development';
-const config=require('./knexfile.js');
-const knex=require('knex')(config);
+const express = require('express');
+const app = express();
 
-const sql=knex('artist');
+const env = 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
 
-sql.then((result)=>{
-  console.log(result);
-  knex.destroy();
-});
+app.use(express.static('public'));
 
-sql.catch((err)=>{
-  console.error(err);
-  knex.destroy();
-  process.exit(1);
+function queryKnex(table, constraints = {}) {
+  for (let column in constraints) {
+    let sql = knex(table).where(`${table}.${column}`, constraints[column])
+      .then((sqlRes) => {
+        knex.destroy();
+        return sqlRes;
+      })
+      .catch((err) => {
+        console.error(err);
+        knex.destroy();
+        process.exit(1);
+      });
+    return sql;
+  }
+}
+
+function genre(req, res) {
+  var query = req.query;
+  let answer = queryKnex('genre', query);
+  answer.then((found) => {
+    res.send(found);})
+    .catch((err)=>{
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+function artist(req,res) {
+  var query = req.query;
+  let answer = queryKnex('artist', query);
+  answer.then((found) => {
+    res.send(found);})
+    .catch((err)=>{
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+function song(req,res) {
+  var query = req.query;
+  console.log('query:',query);
+  let answer = queryKnex('song', query);
+  answer.then((found) => {
+    res.send(found);})
+    .catch((err)=>{
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+function user(req,res) {
+  var query = req.query;
+  let answer = queryKnex('users', query);
+  answer.then((found) => {
+    res.send(found);})
+    .catch((err)=>{
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+app.get('/genre/', genre);
+app.get('/artist/', artist);
+app.get('/song/', song);
+app.get('/user/', user);
+
+app.listen(8000, () => {
+  console.log('listening at port 8000');
 });
--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 module.exports={
   development: {
     client: 'pg',
-    connection: 'process.env.PG_CONNECTION_STRING',
+    connection: 'postgres://localhost/musical_taste',
     searchPath: 'knex,public'
   }
 };
